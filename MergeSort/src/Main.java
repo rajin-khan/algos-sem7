82ecdeec0a04cd753a77758732bfb3d0d8ac44f8
Rajin Khan <rajin.khan2001@gmail.com>
2024-04-07 00:45:48 +0600

    Finish MergeSort implementation

--- MergeSort/src/Main.java
+++ MergeSort/src/Main.java
@@ -1,7 +1,15 @@
+import java.util.*;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] names) {
+
+        int[] numbers = { 6, 7, 4, 5, 6, 3, 9, 2 };
+
+        var sorter = new MergeSort();
+
+        sorter.sort(numbers);
         
-        System.out.println("Hello, World!");;
+        System.out.println(Arrays.toString(numbers));
         
     }
 }
---
+++ MergeSort/src/MergeSort.java
@@ -0,0 +1,69 @@
+public class MergeSort {
+
+    public void sort(int[] array) {
+
+        if (array.length<2) {
+
+            return;
+        }
+
+        //Divide each half
+        var middle = array.length/2;
+        int[] left = new int[middle];
+
+        for (var i=0; i<middle; i++) {
+
+            left[i] = array[i];
+        }
+
+        int[] right = new int[array.length-middle];
+
+        for (var i=middle; i<array.length; i++) {
+
+            right[i-middle] = array[i];
+        }
+
+        //sort each half
+        sort(left);
+        sort(right);
+
+        //Merge the result
+        merge(left, right, array);
+
+    }
+    
+    public void merge(int[] left, int[] right, int[] result) {
+
+        int i=0, j=0, k=0;
+
+        while (i<left.length && j<right.length) {
+
+            if (left[i] <= right[j]) {
+
+                result[k] = left[i];
+                k++;
+                i++;
+            }
+            else {
+
+                result[k] = right[j];
+                k++;
+                j++;
+            }
+        }
+
+        while(i<left.length) {
+
+            result[k] = left[i];
+            k++;
+            i++;
+        }
+        
+        while(j<right.length) {
+
+            result[k] = right[j];
+            k++;
+            j++;
+        }
+    }
+}
